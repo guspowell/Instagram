c29cb7a7e9121d322bff2f929082d56171a06a16
guspowell <augustuspowell@me.com>
2015-02-21 19:13:36 +0000

    broken migrations by adding restaurants when generating paperclip

---
+++ spec/features/photos_feature_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+feature 'photos' do
+
+  context 'no photos have been added' do
+
+    scenario 'should have a prompt to add a photo' do
+      visit '/photos'
+      expect(page).to have_content 'No photos'
+      expect(page).to have_link 'Add photo'
+    end
+
+  end
+
+  context 'photos have been added' do
+
+    before do
+      Photo.create(caption: 'a nice view')
+    end
+
+    scenario 'display photos' do
+      visit '/photos'
+      expect(page).to have_content('a nice view')
+      expect(page).not_to have_content('No photos yet')
+    end
+
+  end
+
+  context 'creating photos' do
+
+    scenario 'prompts user to fill out a form, then displays the photo' do
+      visit '/photos'
+      click_link 'Add photo'
+      fill_in 'Caption', with: 'a nice view'
+      click_button 'Create Photo'
+      expect(page).to have_content 'a nice view'
+      expect(current_path).to eq '/photos'
+    end
+
+  end
+
+end
